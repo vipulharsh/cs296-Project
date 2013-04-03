3b4240b4b2259ad8075f8b4f08eea4d6550c6677
ALOK YADAV <yadavalok@.(none)>
2013-04-03 15:13:00 +0530

    Initial base code with some changes

---
+++ cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/DependInfo.cmake
@@ -0,0 +1,57 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Collision/Shapes/b2ChainShape.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/Shapes/b2ChainShape.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Collision/Shapes/b2CircleShape.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/Shapes/b2CircleShape.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Collision/Shapes/b2EdgeShape.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/Shapes/b2EdgeShape.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Collision/Shapes/b2PolygonShape.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/Shapes/b2PolygonShape.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Collision/b2BroadPhase.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/b2BroadPhase.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Collision/b2CollideCircle.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/b2CollideCircle.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Collision/b2CollideEdge.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/b2CollideEdge.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Collision/b2CollidePolygon.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/b2CollidePolygon.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Collision/b2Collision.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/b2Collision.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Collision/b2Distance.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/b2Distance.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Collision/b2DynamicTree.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/b2DynamicTree.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Collision/b2TimeOfImpact.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/b2TimeOfImpact.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Common/b2BlockAllocator.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Common/b2BlockAllocator.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Common/b2Draw.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Common/b2Draw.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Common/b2Math.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Common/b2Math.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Common/b2Settings.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Common/b2Settings.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Common/b2StackAllocator.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Common/b2StackAllocator.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Common/b2Timer.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Common/b2Timer.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/Contacts/b2ChainAndCircleContact.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2ChainAndCircleContact.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/Contacts/b2ChainAndPolygonContact.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2ChainAndPolygonContact.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/Contacts/b2CircleContact.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2CircleContact.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/Contacts/b2Contact.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2Contact.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/Contacts/b2ContactSolver.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2ContactSolver.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/Contacts/b2EdgeAndCircleContact.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2EdgeAndCircleContact.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/Contacts/b2EdgeAndPolygonContact.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2EdgeAndPolygonContact.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/Contacts/b2PolygonAndCircleContact.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2PolygonAndCircleContact.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/Contacts/b2PolygonContact.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2PolygonContact.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/Joints/b2DistanceJoint.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2DistanceJoint.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/Joints/b2FrictionJoint.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2FrictionJoint.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/Joints/b2GearJoint.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2GearJoint.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/Joints/b2Joint.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2Joint.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/Joints/b2MouseJoint.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2MouseJoint.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/Joints/b2PrismaticJoint.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2PrismaticJoint.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/Joints/b2PulleyJoint.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2PulleyJoint.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/Joints/b2RevoluteJoint.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2RevoluteJoint.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/Joints/b2RopeJoint.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2RopeJoint.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/Joints/b2WeldJoint.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2WeldJoint.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/Joints/b2WheelJoint.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2WheelJoint.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/b2Body.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2Body.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/b2ContactManager.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2ContactManager.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/b2Fixture.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2Fixture.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/b2Island.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2Island.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/b2World.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2World.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Dynamics/b2WorldCallbacks.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2WorldCallbacks.cpp.o"
+  "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/Box2D/Rope/b2Rope.cpp" "/users/ug11/yadavalok/cs296_base_code/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Rope/b2Rope.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ cs296_base_code/obj/main.d
@@ -0,0 +1,136 @@
+/users/ug11/yadavalok/cs296_base_code/obj/main.o: \
+ /users/ug11/yadavalok/cs296_base_code/src/main.cpp \
+ /users/ug11/yadavalok/cs296_base_code/src/render.hpp \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Box2D.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Common/b2Settings.h \
+ /usr/include/c++/4.4/cassert /usr/include/assert.h \
+ /usr/include/features.h /usr/include/bits/predefs.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/wordsize.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-32.h \
+ /usr/include/c++/4.4/cmath \
+ /usr/include/c++/4.4/i486-linux-gnu/bits/c++config.h \
+ /usr/include/c++/4.4/i486-linux-gnu/bits/os_defines.h \
+ /usr/include/c++/4.4/i486-linux-gnu/bits/cpu_defines.h \
+ /usr/include/c++/4.4/bits/cpp_type_traits.h \
+ /usr/include/c++/4.4/ext/type_traits.h /usr/include/math.h \
+ /usr/include/bits/huge_val.h /usr/include/bits/huge_valf.h \
+ /usr/include/bits/huge_vall.h /usr/include/bits/inf.h \
+ /usr/include/bits/nan.h /usr/include/bits/mathdef.h \
+ /usr/include/bits/mathcalls.h /usr/include/bits/mathinline.h \
+ /usr/include/c++/4.4/bits/cmath.tcc \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Common/b2Draw.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Common/b2Math.h \
+ /usr/include/c++/4.4/cfloat \
+ /usr/lib/gcc/i486-linux-gnu/4.4.3/include/float.h \
+ /usr/include/c++/4.4/cstddef \
+ /usr/lib/gcc/i486-linux-gnu/4.4.3/include/stddef.h \
+ /usr/include/c++/4.4/limits \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Common/b2Timer.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Collision/Shapes/b2CircleShape.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Collision/Shapes/b2Shape.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Common/b2BlockAllocator.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Collision/b2Collision.h \
+ /usr/include/c++/4.4/climits \
+ /usr/lib/gcc/i486-linux-gnu/4.4.3/include-fixed/limits.h \
+ /usr/lib/gcc/i486-linux-gnu/4.4.3/include-fixed/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/posix1_lim.h \
+ /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
+ /usr/include/bits/posix2_lim.h /usr/include/bits/xopen_lim.h \
+ /usr/include/bits/stdio_lim.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Collision/Shapes/b2EdgeShape.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Collision/Shapes/b2ChainShape.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Collision/Shapes/b2PolygonShape.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Collision/b2BroadPhase.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Collision/b2DynamicTree.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Common/b2GrowableStack.h \
+ /usr/include/c++/4.4/cstring /usr/include/string.h \
+ /usr/include/xlocale.h /usr/include/bits/string3.h \
+ /usr/include/c++/4.4/algorithm /usr/include/c++/4.4/bits/stl_algobase.h \
+ /usr/include/c++/4.4/bits/functexcept.h \
+ /usr/include/c++/4.4/exception_defines.h \
+ /usr/include/c++/4.4/ext/numeric_traits.h \
+ /usr/include/c++/4.4/bits/stl_pair.h /usr/include/c++/4.4/bits/move.h \
+ /usr/include/c++/4.4/bits/concept_check.h \
+ /usr/include/c++/4.4/bits/stl_iterator_base_types.h \
+ /usr/include/c++/4.4/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/4.4/bits/stl_iterator.h \
+ /usr/include/c++/4.4/debug/debug.h /usr/include/c++/4.4/bits/stl_algo.h \
+ /usr/include/c++/4.4/cstdlib /usr/include/stdlib.h \
+ /usr/include/bits/waitflags.h /usr/include/bits/waitstatus.h \
+ /usr/include/endian.h /usr/include/bits/endian.h \
+ /usr/include/bits/byteswap.h /usr/include/sys/types.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/time.h /usr/include/sys/select.h /usr/include/bits/select.h \
+ /usr/include/bits/sigset.h /usr/include/bits/time.h \
+ /usr/include/sys/sysmacros.h /usr/include/bits/pthreadtypes.h \
+ /usr/include/alloca.h /usr/include/bits/stdlib.h \
+ /usr/include/c++/4.4/bits/algorithmfwd.h \
+ /usr/include/c++/4.4/initializer_list \
+ /usr/include/c++/4.4/bits/stl_heap.h \
+ /usr/include/c++/4.4/bits/stl_tempbuf.h \
+ /usr/include/c++/4.4/bits/stl_construct.h /usr/include/c++/4.4/new \
+ /usr/include/c++/4.4/exception \
+ /usr/include/c++/4.4/bits/stl_uninitialized.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Collision/b2Distance.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Collision/b2TimeOfImpact.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Dynamics/b2Body.h \
+ /usr/include/c++/4.4/memory /usr/include/c++/4.4/bits/allocator.h \
+ /usr/include/c++/4.4/i486-linux-gnu/bits/c++allocator.h \
+ /usr/include/c++/4.4/ext/new_allocator.h \
+ /usr/include/c++/4.4/bits/stl_raw_storage_iter.h \
+ /usr/include/c++/4.4/backward/auto_ptr.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Dynamics/b2Fixture.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Dynamics/b2WorldCallbacks.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Dynamics/b2TimeStep.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Dynamics/b2World.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Common/b2StackAllocator.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Dynamics/b2ContactManager.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Dynamics/Contacts/b2Contact.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2DistanceJoint.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2Joint.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2FrictionJoint.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2GearJoint.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2WheelJoint.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2MouseJoint.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2PrismaticJoint.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2PulleyJoint.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2RevoluteJoint.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2RopeJoint.h \
+ /users/ug11/yadavalok/cs296_base_code/external/include/Box2D/Dynamics/Joints/b2WeldJoint.h \
+ /users/ug11/yadavalok/cs296_base_code/src/cs296_base.hpp \
+ /users/ug11/yadavalok/cs296_base_code/src/callbacks.hpp \
+ /usr/include/GL/glui.h /usr/include/GL/glut.h \
+ /usr/include/GL/freeglut_std.h /usr/include/GL/gl.h \
+ /usr/include/GL/glext.h /usr/include/inttypes.h /usr/include/stdint.h \
+ /usr/include/bits/wchar.h /usr/include/GL/glu.h \
+ /usr/include/c++/4.4/cstdio /usr/include/stdio.h /usr/include/libio.h \
+ /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/lib/gcc/i486-linux-gnu/4.4.3/include/stdarg.h \
+ /usr/include/bits/sys_errlist.h /usr/include/bits/stdio.h \
+ /usr/include/bits/stdio2.h /usr/include/c++/4.4/string \
+ /usr/include/c++/4.4/bits/stringfwd.h \
+ /usr/include/c++/4.4/bits/char_traits.h \
+ /usr/include/c++/4.4/bits/postypes.h /usr/include/c++/4.4/cwchar \
+ /usr/include/bits/wchar2.h /usr/include/c++/4.4/bits/localefwd.h \
+ /usr/include/c++/4.4/i486-linux-gnu/bits/c++locale.h \
+ /usr/include/c++/4.4/clocale /usr/include/locale.h \
+ /usr/include/bits/locale.h /usr/include/c++/4.4/iosfwd \
+ /usr/include/c++/4.4/cctype /usr/include/ctype.h \
+ /usr/include/c++/4.4/bits/ostream_insert.h \
+ /usr/include/c++/4.4/cxxabi-forced.h \
+ /usr/include/c++/4.4/bits/stl_function.h \
+ /usr/include/c++/4.4/backward/binders.h \
+ /usr/include/c++/4.4/bits/basic_string.h \
+ /usr/include/c++/4.4/ext/atomicity.h \
+ /usr/include/c++/4.4/i486-linux-gnu/bits/gthr.h \
+ /usr/include/c++/4.4/i486-linux-gnu/bits/gthr-default.h \
+ /usr/include/pthread.h /usr/include/sched.h /usr/include/bits/sched.h \
+ /usr/include/signal.h /usr/include/bits/setjmp.h /usr/include/unistd.h \
+ /usr/include/bits/posix_opt.h /usr/include/bits/environments.h \
+ /usr/include/bits/confname.h /usr/include/getopt.h \
+ /usr/include/bits/unistd.h \
+ /usr/include/c++/4.4/i486-linux-gnu/bits/atomic_word.h \
+ /usr/include/c++/4.4/bits/basic_string.tcc /usr/include/c++/4.4/vector \
+ /usr/include/c++/4.4/bits/stl_vector.h \
+ /usr/include/c++/4.4/bits/stl_bvector.h \
+ /usr/include/c++/4.4/bits/vector.tcc
