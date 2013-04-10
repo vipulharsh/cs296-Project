55fa1e9caebe4db8ccd94e9a8dcb841f586e65b9
Vipul Harsh <vipulharsh.iit@gmail.com>
2013-04-10 23:16:24 +0530

    Readme

--- cs296_base_code/README.txt
+++ cs296_base_code/README.txt
@@ -1,8 +1,22 @@
-To build the base code do the following:
+Group No - 12
+Alok Yadav 110050043
+Vipul Harsh 110050034
+Siddhant Mittal 110050015
 
-1. Go into the external/src folder and read the README there to build Box2D first.
 
-2. Return back to the cs296_base_code folder and type 'make' to compile.
+We certify that all work done in this assignment is original and hasn't been copied or plagiarized from anywhere.
 
-3. Type 'bin/cs296_base' to run the program.
+WebPage
+www.cse.iitb.ac.in/~sid/project_webpage.html
+
+
+Citations
+
+-Python Tutorials for Matplotlib and html generating script(http://docs.python.org/3/tutorial/)
+-Perf Wiki (http://www.baptiste-wicht.com/2011/07/profile-applications-linux-perf-tools/)
+-GNU make manual(https://www.gnu.org/software/make/manual/html_node/index.html#Top)
+-Doxygen Manual(www.stack.nl/~dimitri/doxygen/manual/index.html)
+-Box2D user Manual(http://www.box2d.org/manual.html)
+-Google    (for being our number one everyday source of information)
+-stack overflow     ( for answering our silliest of questions)
 
--- cs296_base_code/src/cs296_base.hpp
+++ cs296_base_code/src/cs296_base.hpp
@@ -35,66 +35,114 @@
 namespace cs296
 {
 
-  //! What is the difference between a class and a struct in C++?
+  //! What is the difference between a class and a struct in C++? \n
+  //! Members of a class are private by default and members of a struct are private by default
   class base_sim_t;
   struct settings_t;
   
-  //! Why do we use a typedef
+  //! Why do we use a typedef?   \n
+  //! 	In this context , its used to give a different alias for a data type . Its also used to declare a general data type ,when we intend a  	  block of code/function to be used for many data types. 
   typedef base_sim_t* sim_create_fcn(); 
 
   //! Simulation settings. Some can be controlled in the GUI.
+/**
+//center of simulation is set to (0,20) absolute position. \n
+    \a  hz(60.0f),                     : frequency of stepping set to 60\n
+    \a  velocity_iterations(8),        : No of velocity_iterations set to 8 \n
+    \a  position_iterations(3),        : No of position-iterations set to 3 \n
+    \a  draw_shapes(1),                : enable drawing of shapes \n
+    \a  draw_joints(1),                : enable drawing of joints \n
+    \a  draw_AABBs(0),                 : enable drawing of AABB's \n
+    \a  draw_pairs(0),                 : disable drawing of pairs \n
+    \a draw_contact_points(0),         : disable drawing of contact points \n
+    \a  draw_contact_normals(0),       : disable drawing of contact normals \n
+    \a  draw_contact_forces(0),        : disable drawing of contact forces \n
+    \a draw_friction_forces(0),        : disable drawing of friction forces \n
+    \a  draw_COMs(0),                  : disable drawing of \n
+    \a  draw_stats(0),                 : disable drawing of stats \n
+    \a draw_profile(0),                : disable drawing of profile \n
+    \a  enable_warm_starting(1),       : enable \n
+    \a  enable_continuous(1),          : enable \n
+    \a enable_sub_stepping(0),         : enable sub_stepping \n
+    \a  pause(0),                      : disable pause \n
+    \a  single_step(0)                 : disable single_step \n
+
+
+
+    \a struct sim_t                   : creates the simulation world and starts the simulation 
+ 
+
+    \a struct contact_point_t         : stores the information(such as normal , position etc) relating to a contact point between two fixtures.
+
+
+   \a  virtual void step(settings_t* settings) : Callback function for the step button  \n \n
+   \a  virtual void keyboard(unsigned char key) : Not of much significance \n \n
+   \a  virtual void keyboard_up(unsigned char key) : Not of much significance  \n \n
+   \a  void shift_mouse_down(const b2Vec2& p): Not of much significance  \n \n
+   \a  virtual void mouse_down(const b2Vec2& p) : Not of much significance \n \n
+   \a  virtual void mouse_up(const b2Vec2& p) : Not of much significance \n \n
+   \a void mouse_move(const b2Vec2& p) : Not of much significance \n \n
+   \a virtual void pre_solve(b2Contact* contact, const b2Manifold* oldManifold) : callback function when two bodies are about to collide.
+
+
+   
+*/
+
   struct settings_t
   {
     //! Notice the initialization of the class members in the constructor
     //! How is this happening?
     settings_t() :
-      view_center(0.0f, 20.0f),
-      hz(60.0f),
-      velocity_iterations(8),
-      position_iterations(3),
-      draw_shapes(1),
-      draw_joints(1),
-      draw_AABBs(0),
-      draw_pairs(0),
-      draw_contact_points(0),
-      draw_contact_normals(0),
-      draw_contact_forces(0),
-      draw_friction_forces(0),
-      draw_COMs(0),
-      draw_stats(0),
-      draw_profile(0),
-      enable_warm_starting(1),
-      enable_continuous(1),
-      enable_sub_stepping(0),
-      pause(0),
-      single_step(0)
+      view_center(0.0f, 20.0f),      //!center of simulation is set to (0,20) absolute position.
+      hz(60.0f),                     //!sets the frequency of the simulation to 60
+      velocity_iterations(8),        //!No of velocity_iterations set to 8
+      position_iterations(3),        //!No of position-iterations set to 3
+      draw_shapes(1),                //!enable drawing of shapes 
+      draw_joints(1),                //!enable drawing of joints 
+      draw_AABBs(0),                 //!enable drawing of AABB's
+      draw_pairs(0),                 //!disable drawing of pairs
+      draw_contact_points(0),        //!disable drawing of contact points
+      draw_contact_normals(0),       //!disable drawing of contact normals
+      draw_contact_forces(0),        //!disable drawing of contact forces
+      draw_friction_forces(0),       //!disable drawing of friction forces
+      draw_COMs(0),                  //!disable drawing of 
+      draw_stats(0),                 //!disable drawing of stats
+      draw_profile(0),               //!disable drawing of profile
+      enable_warm_starting(1),       //!enable 
+      enable_continuous(1),          //!enable
+      enable_sub_stepping(0),        //!enable sub_stepping
+      pause(0),                      //!disable pause
+      single_step(0)                 //!disable single_step
     {}
     
-    b2Vec2 view_center;
-    float32 hz;
-    int32 velocity_iterations;
-    int32 position_iterations;
-    int32 draw_shapes;
-    int32 draw_joints;
-    int32 draw_AABBs;
-    int32 draw_pairs;
-    int32 draw_contact_points;
-    int32 draw_contact_normals;
-    int32 draw_contact_forces;
-    int32 draw_friction_forces;
-    int32 draw_COMs;
-    int32 draw_stats;
-    int32 draw_profile;
-    int32 enable_warm_starting;
-    int32 enable_continuous;
-    int32 enable_sub_stepping;
-    int32 pause;
-    int32 single_step;
+    b2Vec2 view_center;              //!<center of simulation - absolute position.
+    float32 hz;                      //!<frequency of stepping
+    int32 velocity_iterations;       //!<No of velocity iterations 
+    int32 position_iterations;       //!<No of position iterations   
+    int32 draw_shapes;               //!< disable/enable drawing of shapes   
+    int32 draw_joints;               //!< disable/enable drawing of joints 
+    int32 draw_AABBs;	             //!< disable/enable drawing of AABB's
+    int32 draw_pairs;                //!< disable/enable drawing of pairs
+    int32 draw_contact_points;       //!< disable/enable drawing of contact points
+    int32 draw_contact_normals;      //!< disable/enable drawing of contact normals
+    int32 draw_contact_forces;       //!< disable/enable drawing of contact forces
+    int32 draw_friction_forces;      //!< disable/enable drawing of  frictional forces
+    int32 draw_COMs;                 //!< disable/enable drawing of Centre of Mass         
+    int32 draw_stats;                //!< disable/enable drawing of stats
+    int32 draw_profile;              //!< disable/enable drawing of profile 
+    int32 enable_warm_starting;      //!< disable/enable drawing of warm_starting
+    int32 enable_continuous;         //!< disable/enable drawing of continous
+    int32 enable_sub_stepping;       //!< disable/enable drawing of sub-stepping
+    int32 pause;                     //!< disable/enable drawing of pause
+    int32 single_step;               //!< disable/enable drawing of single step
   };
   
+
+//! \a struct sim_t                   : creates the simulation world and starts the simulation 
+
   struct sim_t
   {
-    const char *name;
+    const char *name;            //!< name of the simulation world
     sim_create_fcn *create_fcn;
 
     sim_t(const char *_name, sim_create_fcn *_create_fcn): 
@@ -105,9 +153,11 @@ namespace cs296
   
   
   const int32 k_max_contact_points = 2048;  
-  struct contact_point_t
+  
+//!  \a struct contact_point_t         : stores the information(such as normal , position etc) relating to a contact point between two fixtures.
+struct contact_point_t
   {
-    b2Fixture* fixtureA;
+    b2Fixture* fixtureA;            
     b2Fixture* fixtureB;
     b2Vec2 normal;
     b2Vec2 position;
@@ -120,30 +170,44 @@ namespace cs296
     
     base_sim_t();
 
-    //! Virtual destructors - amazing objects. Why are these necessary?
+    //! Virtual destructors - amazing objects. Why are these necessary? \n
+    //! Virtual destructors are useful when we have to delete an object of a class through a pointer. If we don't use virtual destructors , 
+    //! the memory alloted for the object never gets deallocated.
     virtual ~base_sim_t();
+    
+    
+    inline b2World* get_world(void){ return m_world;} 
+    
     
     void set_text_line(int32 line) { m_text_line = line; }
     void draw_title(int x, int y, const char *string);
-    
+    //! callback function of the step button
     virtual void step(settings_t* settings);
-
+    //! This function is not being used.
     virtual void keyboard(unsigned char key) { B2_NOT_USED(key); }
+    //! This function is not being used.
     virtual void keyboard_up(unsigned char key) { B2_NOT_USED(key); }
-
+    //! This function is not being used.
     void shift_mouse_down(const b2Vec2& p) { B2_NOT_USED(p); }
+    //! This function is not being used.
     virtual void mouse_down(const b2Vec2& p) { B2_NOT_USED(p); }
+    //! This function is not being used.  
     virtual void mouse_up(const b2Vec2& p) { B2_NOT_USED(p); }
+    //! This function is not being used.
     void mouse_move(const b2Vec2& p) { B2_NOT_USED(p); }
 
-    
+    //! This function is not being used.
     // Let derived tests know that a joint was destroyed.
     virtual void joint_destroyed(b2Joint* joint) { B2_NOT_USED(joint); }
     
     // Callbacks for derived classes.
+    //! This function is not being used.
     virtual void begin_contact(b2Contact* contact) { B2_NOT_USED(contact); }
+    //! This function is not being used.
     virtual void end_contact(b2Contact* contact) { B2_NOT_USED(contact); }
+    //! This function is not being used.
     virtual void pre_solve(b2Contact* contact, const b2Manifold* oldManifold);
+    //! callback function when two bodies are about to collide
     virtual void post_solve(const b2Contact* contact, const b2ContactImpulse* impulse)
     {
       B2_NOT_USED(contact);
@@ -151,13 +215,15 @@ namespace cs296
     }
 
   //!How are protected members different from private memebers of a class in C++ ?
+  //!Private members are accessible nowhere outside the class , but a protected member is accessible from within a derieved class
   protected:
 
-    //! What are Friend classes?
+    //! What are Friend classes? \n
+    //! A Friend class of a class can access the private and protected members of the class
     friend class contact_listener_t;
     
     b2Body* m_ground_body;
-    b2AABB m_world_AABB;
+    b2AABB m_world_AABB;//!
     contact_point_t m_points[k_max_contact_points];
     int32 m_point_count;
 
